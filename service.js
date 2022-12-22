5f9631888e0297423a598f76e24f149dd480b1b3
AsP3X <services@vorberg-webmail.de>
2022-12-22 11:12:42 +0100

    TASK: added service logger and device identifier

--- service.js
+++ service.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
+
+// Loading custom modules
+const Logger = require('./assets/utils/logger');
+
+// Create the logger
+const logger = new Logger("webserver");
+
+logger.log("Booting up microservice...");
 
 // Set the port to listen on
 const port = 3000;
@@ -9,6 +18,21 @@ app.use(express.static('public'));
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
+
+const DIDcache = [];
+
+// log every access to the site
+app.use((req, res, next) => {
+
+  const headers = req.headers;
+  const deviceId = crypto.createHash('sha256').update(JSON.stringify(headers)).digest('hex');
+  
+  logger.log(`Request received with id: ${deviceId}`);
+  if (DIDcache.includes(deviceId)) { logger.log(`${deviceId} is already cached`) }
+
+  DIDcache.push(deviceId);
+  next();
+});
 
 // Set the routes for the app
 app.get('/', (req, res) => {
